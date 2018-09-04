c9a46d4b439cb3429313aae64914a70a436b4cee
3noki <WillSeligman@gmail.com>
2018-09-04 17:29:15 -0600

    map full screan plus a few markers

--- src/App.js
+++ src/App.js
@@ -4,10 +4,33 @@ import './App.css'
 let map;
 
 window.initMap = function() {
-   console.log('loaded')
     map = new window.google.maps.Map(document.getElementById('map'), {
       center: {lat: 40.2244, lng: -105.2689},
       zoom: 16
+    })
+    let pinball = {lat: 40.224428, lng: -105.268916};
+    let marker0 = new window.google.maps.Marker({
+      position: pinball,
+      map: map,
+      title: 'Lyons Classic Pinball'
+    })
+    let bluegrass = {lat: 40.2269, lng: -105.2733};
+    let marker1 = new window.google.maps.Marker({
+      position: bluegrass,
+      map: map,
+      title: 'Planet Bluegrass'
+    })
+    let stonecup = {lat: 40.2245, lng: -105.2706};
+    let marker3 = new window.google.maps.Marker({
+      position: stonecup,
+      map: map,
+      title: 'The Stone Cup'
+    })
+    let wateringhole = {lat: 40.2227, lng: -105.2650};
+    let marker4 = new window.google.maps.Marker({
+      position: wateringhole,
+      map: map,
+      title: 'The Quarry Self-Serve Watering Hole '
     })
   }
 
@@ -25,14 +48,11 @@ componentDidMount () {
    script.defer = true
    script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAWiSZ2beXFrSFWzZVRgF122wCkVf4P67Y&v=3.32&callback=initMap"
    document.head.appendChild(script)
-
 }
 
   render() {
     return (
-      <div>{this.map}
-      <div style={{ width: 500, height: 500}} id="map" />
-      </div>
+      <div id="map" />
     )
   }
 }
